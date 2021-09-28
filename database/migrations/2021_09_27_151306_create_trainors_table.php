f08e2003252717e846cec61cd2c2d349054e98cb
jay calamba <jaycalambastudio@gmail.com>
2021-09-28 07:01:50 +0700

    databases

---
+++ database/migrations/2021_09_27_151306_create_trainors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainors', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('users_id')->unsigned();
+            $table->string('specialty');
+            $table->timestamps();
+            //foreign key
+            $table->foreign('users_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainors');
+    }
+}
---
+++ database/migrations/2021_09_27_151717_create_trainees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTraineesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainees', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('users_id')->unsigned();
+            $table->bigInteger('courses_id')->unsigned();
+            $table->string('status', 30);
+            $table->timestamps();
+            //foreign key
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('courses_id')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainees');
+    }
+}
